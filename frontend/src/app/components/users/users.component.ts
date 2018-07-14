1f8f9adb5ff9bd39f4965d4a4642bdf04748deea
Andres Carrillo <acarrillo761@unab.edu.co>
2018-07-13 19:49:57 -0500

    resetFor y addForm y arreglo de errores de datos indefinidos

--- frontend/src/app/components/users/users.component.ts
+++ frontend/src/app/components/users/users.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from "../../services/user.service";
+import { NgForm } from '@angular/forms';
+import { User } from '../../models/user';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -11,6 +13,21 @@ export class UsersComponent implements OnInit {
   constructor(private userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  addUser(form: NgForm ){
+    this.userService.postUser(form.value).subscribe(res=>{
+      console.log(res)
+    })
+
+  }
+
+  resetForm(form?: NgForm){
+    if(form){
+      form.reset();
+      this.userService.selectedUser = new User();
+    }
+
   }
 
 }
