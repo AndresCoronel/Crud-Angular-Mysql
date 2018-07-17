3b327bb1749420fce5867c7addc4631b67111e2c
Andres Carrillo <acarrillo761@unab.edu.co>
2018-07-17 14:46:20 -0500

    agregado las funciones del cliente-producto services

--- frontend/src/app/components/cliente-producto/cliente-producto.component.ts
+++ frontend/src/app/components/cliente-producto/cliente-producto.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import { Cliente } from '../../models/cliente';
+import { ClienteProductoService } from '../../services/cliente_producto/cliente-producto.service';
+declare var M: any;
 @Component({
-  selector: 'app-cliente-producto',
-  templateUrl: './cliente-producto.component.html',
-  styleUrls: ['./cliente-producto.component.css']
+  selector: 'app-users',
+  templateUrl: './clientes.component.html',
+  styleUrls: ['./clientes.component.css'],
+  providers: [ClienteProductoService]
 })
 export class ClienteProductoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private clienteproductoService: ClienteProductoService) { }
 
   ngOnInit() {
+    this.getClientesProductos();
+  }
+  getClientesProductos() {
+    this.clienteproductoService.getClientesProductos()
+      .subscribe(res => {
+        this.clienteproductoService.clientes = res as Cliente[];
+      });
+  }
+  addCliente(form: NgForm ){
+    console.log("esto llego del form" + form.value.id)
+    if(form.value.id){
+      this.clienteproductoService.putCliente(form.value).subscribe(res=>{
+        console.log(res);
+        this.resetForm(form);
+        M.toast({html: 'Cliente actualizado'})
+        this.getClientesProductos();
+     
+      })
+    }else{
+      this.clienteproductoService.postCliente(form.value).subscribe(res=>{
+        console.log(res);
+        this.resetForm(form);
+        M.toast({html: 'Cliente agregado'})
+        this.getClientesProductos();
+      })
+    }
+    
+
+  }
+  deletedCliente(id: number ){
+    if(confirm('¿Quieres eliminar este cliente?')){
+    this.clienteproductoService.deleteCliente(id)
+    .subscribe(res=>{
+      M.toast({html: 'Cliente eliminado'})
+      this.getClientesProductos();  
+    } )
+  }}
+
+  editCliente(cliente: Cliente){
+    this.clienteproductoService.selectedCliente = cliente;
+
   }
 
+
+  resetForm(form?: NgForm){
+    if(form){
+      form.reset();
+      this.clienteproductoService.selectedCliente = new Cliente();
+    }
+
+  }
+  
 }
--- frontend/src/app/services/cliente_producto/cliente-producto.service.ts
+++ frontend/src/app/services/cliente_producto/cliente-producto.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { ClienteProducto } from '../../models/clienteproducto';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteProductoService {
+  selectedClienteProducto: ClienteProducto;
+  clientesproductos: ClienteProducto[];
+  readonly URL_API = 'http://localhost:3000/clienteproducto';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.selectedClienteProducto = new ClienteProducto();
+  }
+
+  getClientesProductos() {
+    return this.http.get(this.URL_API);
+   }
+
+  postClientesProductos(ClienteProducto: ClienteProducto) {
+    return this.http.post(this.URL_API, ClienteProducto);
+   }
+
+   putProducto(clienteproducto: ClienteProducto){
+    return this.http.put(this.URL_API+ `/${clienteproducto.id}`, clienteproducto);
+  }
+
+  deleteProducto(id: number) {
+    return this.http.delete(this.URL_API+`/${id}`);
+   }
+
 }
